7507e3d6643febe968445d2483ba4496a68e56b5
Luis <luisperi18@hotmail.com>
2023-05-20 15:32:25 +0200

    Add index logic

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import "./style.css";
+import { Header } from "./components/header";
+import { Main } from "./components/main";
+import { Footer } from "./components/footer";
+
+new Header("#app");
+new Main("#app");
+new Footer("#app");
