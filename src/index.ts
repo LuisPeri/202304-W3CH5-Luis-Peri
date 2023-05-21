397765276d98b6ebddf800c0993de720e4c3b060
Luis <luisperi18@hotmail.com>
2023-05-21 16:10:26 +0200

    Add menu to maints

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import { Header } from "./components/header";
 import { Card } from "./components/cards";
 import { Main } from "./components/main";
 import { Footer } from "./components/footer";
+import { Menu } from "./components/menu";
 
 new Header("#app");
 new Card("#app");
 new Main("#app");
+new Menu("#app");
 new Footer("#app");
