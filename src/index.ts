48de48841cb7073b90559c47cc03e0486a06c8c6
Luis <luisperi18@hotmail.com>
2023-05-21 15:39:35 +0200

    Add card to maints

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import "./style.css";
 import { Header } from "./components/header";
+import { Card } from "./components/cards";
 import { Main } from "./components/main";
-import { try } from "./data/try"
 import { Footer } from "./components/footer";
 
 new Header("#app");
+new Card("#app");
 new Main("#app");
 new Footer("#app");
